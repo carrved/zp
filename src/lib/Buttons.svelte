319b434d52ad5cfc1765fc146924483e3d107f47
VexdStudios <36055611+Zaxiify@users.noreply.github.com>
2022-09-08 01:56:36 -0500

    features | fixed download tm client btuton

--- src/lib/Buttons.svelte
+++ src/lib/Buttons.svelte
@@ -1,28 +1,10 @@
 <script>
     import "fluent-svelte/theme.css";
     import { Button } from 'fluent-svelte';
-
-    function downloadFile(url, fileName) {
-        fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-            .then(res => res.blob())
-            .then(res => {
-            const aElement = document.createElement('a');
-            aElement.setAttribute('download', fileName);
-            const href = URL.createObjectURL(res);
-            aElement.href = href;
-            aElement.setAttribute('target', '_blank');
-            aElement.click();
-            URL.revokeObjectURL(href);
-        });
-    };
-
-    const downloadTmClient = () => {
-        console.log('trying to download tm client...');
-        downloadFile("./lib/totalmath.html", "tm.html");
-        console.log('downloaded')
-    }
 </script>
 
-<Button variant="accent" on:click={downloadTmClient}>
-    tm client
+<Button variant="accent">
+    <a href='./lib/totalmath.html' download>
+        download tm
+    </a>
 </Button>
